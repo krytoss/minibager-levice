db92a15b09ad05ccec30e27ef68048b0836a1950
Ludovit Kopcsanyi <xkopcsan@fi.muni.cz>
2018-08-26 10:44:34 +0200

    Menu edit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,7 @@
     <head>
         <title>Mini-bager levice - @yield('title')</title>
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900&amp;subset=latin-ext" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/css/bootstrap.min.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/css/bootstrap-grid.min.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
