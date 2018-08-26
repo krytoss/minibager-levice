cae2a8538b9f5615463cee7d5abdb69c00cd03c0
Ludovit Kopcsanyi <xkopcsan@fi.muni.cz>
2018-08-26 10:42:55 +0200

    Menu edit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,10 @@
     <body>
 
         <header id="main-header">
+
+            <div class="phone">
+                <i class="fa fa-mobile" aria-hidden="true"></i><a href="tel:+421903222333">0912 345 678</a>
+            </div>
             
             <div id="main-menu" class="menu horizontal">
                 
